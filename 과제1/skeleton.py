3b0f01fbeff1a1f135f36c0bd294a5b327328e2a
Inhyuk Jeong <jeongih822@g.skku.edu>
2024-03-08 14:42:50 +0900

    assignment1 upload

--- 과제1/skeleton.py
+++ 과제1/skeleton.py
@@ -3,11 +3,9 @@ import numpy as np
 
 def draw_checkerboard(img, N, M):
     """
-    Please write a code here
+    Please write code here
     """
     
-            
-            
 
 if __name__ == "__main__":
     
@@ -18,8 +16,19 @@ if __name__ == "__main__":
     img = np.full((N, N, 1), 255, np.uint8)
     
     draw_checkerboard(img, N, M)
+
+    """
+    Please write edge detection code here
+    """
+
+    """
+    Please write line detection and line drawing code here
+    """
     
-    cv2.imshow("Result", img)
+    cv2.imshow("Result", final_img)
     
     if cv2.waitKey(0) == 27:
         cv2.destroyAllWindows()
+
+
+        
